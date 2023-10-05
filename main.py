372c2c4eba942e776257c3d264fd34ecb5147ce9
nicolello <nicolamigone179@gmail.com>
2023-10-05 19:47:53 +0200

    Added character count

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ with open('_chat.txt') as f:
     lines = f.readlines()
 
 pplMess = {}
+pplChar = {}
+
+maxNameSize=10
 
 for line in lines:
     # timestamp is between two square brackets like so [9/14/23, 19:13:20]. If it doesn't start this way, ignore.
@@ -14,12 +17,15 @@ for line in lines:
     # Name ends with :
     endName = line.find(':')
     name = line[:endName].replace('\u202f', ' ')
+    maxNameSize=max(maxNameSize, len(name))
     if pplMess.get(name) is None:
         pplMess[name] = 1
+        pplChar[name] = len(line[endName+1:])
     else:
         pplMess[name] += 1
+        pplChar[name] += len(line[endName+1:])
 
 sortedMessages = sorted(pplMess.items(), key=lambda item: item[1], reverse=True)
 
 for name, messages in sortedMessages:
-    print(f"{name}:{messages}")
+    print(f"{name.ljust(maxNameSize+2, ' ')} {messages} messages, {pplChar[name]} characters")
